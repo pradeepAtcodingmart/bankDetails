6ac1b6e86ceebc1db96cd1f1bb952c5f241b4e6f
pradeepAtcodingmart <pradeep@codingmart.com>
2020-05-17 01:52:44 +0530

    Updates

---
+++ precache-manifest.b7716116174fb3b783c05e449ade61a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca72712c6961af2d232d542545405f6",
+    "url": "/bankdetails/index.html"
+  },
+  {
+    "revision": "a4f2bd474e0e87a7b64d",
+    "url": "/bankdetails/static/css/main.04a0a621.chunk.css"
+  },
+  {
+    "revision": "2ea6bd478cbe50821e2a",
+    "url": "/bankdetails/static/js/2.7c71c8b3.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/bankdetails/static/js/2.7c71c8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f2bd474e0e87a7b64d",
+    "url": "/bankdetails/static/js/main.59936108.chunk.js"
+  },
+  {
+    "revision": "ffb5dedccf2c8e81d7f8",
+    "url": "/bankdetails/static/js/runtime-main.c6e3da88.js"
+  }
+]);
